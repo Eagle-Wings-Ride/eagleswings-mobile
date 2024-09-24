9fcf1d9c5ca7ba3cc3887f3029339c7f6345b26f
creedtech <ayodeleayoolaoke4@gmail.com>
2024-09-24 15:36:25 +0100

    adding models to the rider aspect

---
+++ lib/data/models/child_model.dart
@@ -0,0 +1,17 @@
+class ChildModel {
+  final String fullName;
+  final String age;
+  final String grade;
+  final String school;
+  final String address;
+  final String relationship;
+
+  ChildModel({
+    required this.fullName,
+    required this.age,
+    required this.grade,
+    required this.school,
+    required this.address,
+    required this.relationship,
+  });
+}
